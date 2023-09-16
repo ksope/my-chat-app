54129d0ad6cd56b7bc6499193bea1cdf3ef39828
ksope <105855785+ksope@users.noreply.github.com>
2023-09-16 21:07:08 +0100

    Update App.js
    The Firebase configuration code is passed within the App component;
    
    Firebase and Cloud Firestore are initialized after the firebaseConfig object declaration.

--- App.js
+++ App.js
@@ -6,16 +6,36 @@ import Chat from "./components/Chat";
 // import react Navigation
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+//initialize connection to Firestore
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBTRTniwDNVMDstvhiaHBGlRokNbL2iYhE",
+        authDomain: "chat-app-3546a.firebaseapp.com",
+        projectId: "chat-app-3546a",
+        storageBucket: "chat-app-3546a.appspot.com",
+        messagingSenderId: "26443162518",
+        appId: "1:26443162518:web:cfcf2be3e7b7d8e0e973b7",
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    // Initialize Cloud Firestore and get a reference to the service
+    const db = getFirestore(app);
+
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Start">
                 <Stack.Screen name="Start" component={Start} />
-                <Stack.Screen name="Chat" component={Chat} />
+                <Stack.Screen name="Chat">
+                    {(props) => <Chat db={db} {...props} />}
+                </Stack.Screen>
             </Stack.Navigator>
         </NavigationContainer>
     );
