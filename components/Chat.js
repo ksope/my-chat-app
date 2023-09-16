6f3c1131f936e2fc58c6c5e7f5b7ed0356968790
ksope <105855785+ksope@users.noreply.github.com>
2023-09-16 16:18:42 +0100

    Modify Chat.js
    Add onSend() to handle how new messages sent to previous messages are sent
    
    Import the Bubble component and use the renderBubble prop to alter how message bubble are displayed
    
    KeyboardAvoidingView and Platform component is imported to avoid bug in older phones  where keyboard hides messages in screen for android phones

--- components/Chat.js
+++ components/Chat.js
@@ -6,12 +6,35 @@ import {
     KeyboardAvoidingView,
     Platform,
 } from "react-native";
-import { GiftedChat } from "react-native-gifted-chat";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
     const [messages, setMessages] = useState([]);
     const { name, color } = route.params; //extract the name & color properties passed through the route prop object
 
+    //add new messages sent to previous messages sent
+    const onSend = (newMessages) => {
+        setMessages((previousMessages) =>
+            GiftedChat.append(previousMessages, newMessages)
+        );
+    };
+
+    //handle how message bubbles are displayed
+    const renderBubble = (props) => {
+        return (
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    right: {
+                        backgroundColor: "a5a869",
+                    },
+                    left: {
+                        backgroundColor: "#C9CAA5",
+                    },
+                }}
+            />
+        );
+    };
 
     useEffect(() => {
         setMessages([
@@ -36,7 +59,7 @@ const Chat = ({ route, navigation }) => {
 
     useEffect(() => {
         navigation.setOptions({
-            title: name,
+            title: name, //set header title
             headerStyle: {
                 backgroundColor: color, //set the background color
             },
@@ -44,8 +67,18 @@ const Chat = ({ route, navigation }) => {
     }, []);
 
     return (
-        <View style={styles.container}>
-            <Text>Hello {name}</Text>
+        <View>
+            <GiftedChat
+                messages={messages}
+                renderBubble={renderBubble}
+                onSend={(messages) => onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
+            {Platform.OS === "android" ? (
+                <KeyboardAvoidingView behavior="height" /> //avoid bug in older phones where keyboard hides messages in screen for android phones
+            ) : null}
         </View>
     );
 };
