3a7f1f8f3745af58f8191460640bd595fc368fcf
ksope <105855785+ksope@users.noreply.github.com>
2023-09-16 16:00:30 +0100

    Modify Chat.js
    Add messages as a new state to send, receive, and display messages
    
    Add a useEffect to set the message state with a static message so that I can see each element of the UI displayed on the screen immediately

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,38 @@
 import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import {
+    StyleSheet,
+    View,
+    Text,
+    KeyboardAvoidingView,
+    Platform,
+} from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
+    const [messages, setMessages] = useState([]);
     const { name, color } = route.params; //extract the name & color properties passed through the route prop object
+
+
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: "Hello and welcome to the group chat",
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: "React Native",
+                    avatar: "https://placeimg.com/140/140/any",
+                },
+            },
+            {
+                _id: 2,
+                text: "I have entered the Chat",
+                createdAt: new Date(),
+                system: true,
+            },
+        ]);
+    }, []);
 
     useEffect(() => {
         navigation.setOptions({
