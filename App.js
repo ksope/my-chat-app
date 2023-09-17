c5a03b9a0f08e20373cd73d8da2f14113d477c22
ksope <105855785+ksope@users.noreply.github.com>
2023-09-17 21:16:10 +0100

    Update App.js
    Delete unused components{ StatusBar } and { StyleSheet, Text, View }

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 // import the screens
 import Start from "./components/Start";
 import Chat from "./components/Chat";
