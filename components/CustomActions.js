d976a75af1dbaca918c5ebf430b81bf2818b55ed
ksope <105855785+ksope@users.noreply.github.com>
2023-09-21 20:37:03 +0100

    Create CustomActions.js component
    Create CustomActions.js component to render an "action" button

---
+++ components/CustomActions.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {
+    StyleSheet,
+    View,
+    Text,
+    Button,
+    TouchableOpacity,
+} from "react-native";
+
+const CustomActions = ({ wrapperStyle, iconTextStyle }) => {
+    const onActionPress = () => {};
+    return (
+        <TouchableOpacity style={styles.container} onPress={onActionPress}>
+            <Vieww style={[styles.wrapper, wrapperStyle]}>
+                <Text style={[styles.iconText, iconTextStyle]}>+</Text>
+            </Vieww>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        width: 26,
+        height: 26,
+        marginLeft: 10,
+        marginBottom: 10,
+    },
+    wrapper: {
+        borderRadius: 13,
+        borderColor: "#b2b2b2",
+        borderWidth: 2,
+        flex: 1,
+    },
+    iconText: {
+        color: "#b2b2b2",
+        fontWeight: "bold",
+        fontSize: 10,
+        backgroundColor: "transparent",
+        textAlign: "center",
+    },
+});
+
+export default CustomActions;
