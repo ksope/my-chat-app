cd8135d65793352b926d7343620d33b0c138e9dc
ksope <105855785+ksope@users.noreply.github.com>
2023-09-22 20:31:25 +0100

    Update CustomActions.js
    Pass the 'storage' handler as a prop in renderCustomActions()
    
    Declare function newUploadRef() to create a new reference for the Storage Cloud
    
    Modify pickImage() to include the firebase/storage meths upoadBytes() which will be used to upload the image file
     Declare function 'generateReference()' to generate unique references for each image file uploaded
    
    Update pickImage() to use generateReference()
    
    Use the getDownloadURL() function to get the remote URL of image just uploaded
    
    Refactor pickImage(), takePhoto() and create a new function uploadAndSendImage() to handle image file uploads

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,17 +1,13 @@
-import React from 'react'
-import {
-    StyleSheet,
-    View,
-    Text,
-    Button,
-    TouchableOpacity,
-} from "react-native";
-import { useActionSheet } from '@expo/react-native-action-sheet';
+import React from "react";
+import { StyleSheet, View, Text, Button, TouchableOpacity } from "react-native";
+import { useActionSheet } from "@expo/react-native-action-sheet";
 import * as ImagePicker from "expo-image-picker";
 import * as Location from "expo-location";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
-const CustomActions = ({ wrapperStyle, iconTextStyle, onSend }) => {
+const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID }) => {
     const actionSheet = useActionSheet();
+    const newUploadRef = ref(storage, "image123");
     //display set of defined actions when pressed
     const onActionPress = () => {
         const options = [
@@ -43,14 +39,27 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend }) => {
     };
 
     //Pick an image from the library
+    const uploadAndSendImage = async (imageURI) => {
+        const uniqueRefString = generateReference(imageURI);
+        const newUploadRef = ref(storage, uniqueRefString);
+        const response = await fetch(imageURI);
+        const blob = await response.blob();
+        //upload an image file blob using the Firebase Storage method
+        uploadBytes(newUploadRef, blob).then(async (snapshot) => {
+            const imageURL = await getDownloadURL(snapshot.ref);
+            onSend({ image: imageURL });
+        });
+    };
+
+    //pick image from device library
     const pickImage = async () => {
         let permissions =
             await ImagePicker.requestMediaLibraryPermissionsAsync();
         if (permissions?.granted) {
             let result = await ImagePicker.launchImageLibraryAsync();
-            if (!result.canceled) {
-                console.log("uploading and uploading the image occurs here");
-            } else Alert.alert("Permissions haven't been granted.");
+            if (!result.canceled)
+                await uploadAndSendImage(result.assets[0].uri);
+            else Alert.alert("Permissions haven't been granted.");
         }
     };
 
@@ -59,26 +68,17 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend }) => {
         let permissions = await ImagePicker.requestCameraPermissionsAsync();
         if (permissions?.granted) {
             let result = await ImagePicker.launchCameraAsync();
-            if (!result.canceled) {
-                console.log("uploading and uploading the image occurs here");
-            } else Alert.alert("Permissions haven't been granted.");
+            if (!result.canceled)
+                await uploadAndSendImage(result.assets[0].uri);
+            else Alert.alert("Permissions haven't been granted.");
         }
     };
 
-    //Get access to device's location
-    const getLocation = async () => {
-        let permissions = await Location.requestForegroundPermissionsAsync();
-        if (permissions?.granted) {
-            const location = await Location.getCurrentPositionAsync({});
-            if (location) {
-                onSend({
-                    location: {
-                        longitude: location.coords.longitude,
-                        latitude: location.coords.latitude,
-                    },
-                });
-            } else Alert.alert("Error occurred while fetching location");
-        } else Alert.alert("Permissions haven't been granted.");
+    //generate unique reference for each image file uploaded
+    const generateReference = (uri) => {
+        const timeStamp = new Date().getTime();
+        const imageName = uri.split("/")[uri.split("/").length - 1];
+        return `${userID}-${timeStamp}-${imageName}`;
     };
 
     return (
@@ -88,7 +88,7 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend }) => {
             </View>
         </TouchableOpacity>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
